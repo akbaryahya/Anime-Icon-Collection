90fbda87f1597c9491cac27f3f05f7ae1f28e1a6
Akbar Yahya <akbaryahya_ym@yahoo>
2021-06-08 00:01:39 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Rebuild_Icon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.TextBox();
+            this.folder_url = new System.Windows.Forms.TextBox();
+            this.folder_icon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TypeIcon = new System.Windows.Forms.ComboBox();
+            this.is_remove_icon_found = new System.Windows.Forms.CheckBox();
+            this.bt_clear_cache = new System.Windows.Forms.Button();
+            this.is_remove_config = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(423, 300);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(53, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(12, 12);
+            this.Log.Multiline = true;
+            this.Log.Name = "Log";
+            this.Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Log.Size = new System.Drawing.Size(468, 212);
+            this.Log.TabIndex = 1;
+            // 
+            // folder_url
+            // 
+            this.folder_url.Location = new System.Drawing.Point(12, 251);
+            this.folder_url.Name = "folder_url";
+            this.folder_url.Size = new System.Drawing.Size(231, 23);
+            this.folder_url.TabIndex = 2;
+            this.folder_url.Text = "D:\\tes";
+            // 
+            // folder_icon
+            // 
+            this.folder_icon.Location = new System.Drawing.Point(249, 251);
+            this.folder_icon.Name = "folder_icon";
+            this.folder_icon.Size = new System.Drawing.Size(231, 23);
+            this.folder_icon.TabIndex = 3;
+            this.folder_icon.Text = "i.ico";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(249, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Base Icon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Type Folder";
+            // 
+            // TypeIcon
+            // 
+            this.TypeIcon.FormattingEnabled = true;
+            this.TypeIcon.Items.AddRange(new object[] {
+            "Videos",
+            "Pictures",
+            "Music",
+            "Documents",
+            "Generic"});
+            this.TypeIcon.Location = new System.Drawing.Point(12, 301);
+            this.TypeIcon.Name = "TypeIcon";
+            this.TypeIcon.Size = new System.Drawing.Size(231, 23);
+            this.TypeIcon.TabIndex = 7;
+            // 
+            // is_remove_icon_found
+            // 
+            this.is_remove_icon_found.AutoSize = true;
+            this.is_remove_icon_found.Location = new System.Drawing.Point(12, 330);
+            this.is_remove_icon_found.Name = "is_remove_icon_found";
+            this.is_remove_icon_found.Size = new System.Drawing.Size(95, 19);
+            this.is_remove_icon_found.TabIndex = 8;
+            this.is_remove_icon_found.Text = "Remove Icon";
+            this.is_remove_icon_found.UseVisualStyleBackColor = true;
+            // 
+            // bt_clear_cache
+            // 
+            this.bt_clear_cache.Location = new System.Drawing.Point(338, 300);
+            this.bt_clear_cache.Name = "bt_clear_cache";
+            this.bt_clear_cache.Size = new System.Drawing.Size(79, 23);
+            this.bt_clear_cache.TabIndex = 9;
+            this.bt_clear_cache.Text = "Clear Cache";
+            this.bt_clear_cache.UseVisualStyleBackColor = true;
+            this.bt_clear_cache.Click += new System.EventHandler(this.bt_clear_cache_Click);
+            // 
+            // is_remove_config
+            // 
+            this.is_remove_config.AutoSize = true;
+            this.is_remove_config.Location = new System.Drawing.Point(113, 330);
+            this.is_remove_config.Name = "is_remove_config";
+            this.is_remove_config.Size = new System.Drawing.Size(108, 19);
+            this.is_remove_config.TabIndex = 10;
+            this.is_remove_config.Text = "Remove Config";
+            this.is_remove_config.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 356);
+            this.Controls.Add(this.is_remove_config);
+            this.Controls.Add(this.bt_clear_cache);
+            this.Controls.Add(this.is_remove_icon_found);
+            this.Controls.Add(this.TypeIcon);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.folder_icon);
+            this.Controls.Add(this.folder_url);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Rebuild Icon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.TextBox folder_url;
+        private System.Windows.Forms.TextBox folder_icon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox TypeIcon;
+        private System.Windows.Forms.CheckBox is_remove_icon_found;
+        private System.Windows.Forms.Button bt_clear_cache;
+        private System.Windows.Forms.CheckBox is_remove_config;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rebuild_Icon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string url = folder_url.Text;
+            string bas = folder_icon.Text;
+            string type = TypeIcon.Text;
+
+            if (Directory.Exists(url))
+            {
+                log($"~ Found Folder {url} and Set to {type}");
+                int all = 0;
+                int found = 0;
+                int error = 0;
+                int remove_icon = 0;
+                int noremove_icon = 0;
+                int remove_config = 0;
+                DirectoryInfo info = new DirectoryInfo(url);
+                foreach (DirectoryInfo file in info.GetDirectories())
+                {
+                    var dir = file.FullName;
+
+                    var ico = @$"{dir}\{bas}";
+                    var ini = @$"{dir}\desktop.ini";
+                    var hid = @$"{dir}\.hidden";
+
+                    all++;
+                    log(ico);
+                    //Thread.Sleep(600);
+
+                    if (is_remove_config.Checked)
+                    {
+                        RetIcon(dir, bas,is_remove_icon_found.Checked);
+                        remove_config++;
+                    }
+                    else
+                    {
+                        // check file icon
+                        if (File.Exists(ico))
+                        {
+                            try
+                            {
+                                // Making file ini
+                                string[] lines = { "[.ShellClassInfo]", $"IconResource={bas},0", "[ViewState]", "Mode=", "Vid=", $"FolderType={type}" };
+                                File.WriteAllLines(ini, lines);
+
+                                // Making file hide            
+                                string[] linesLinux = { "desktop.ini", bas };
+                                File.WriteAllLines(dir + @"\.hidden", linesLinux);
+
+                                // Making system files and hidden
+                                File.SetAttributes(ini, File.GetAttributes(ini) | FileAttributes.Hidden | FileAttributes.System);
+                                File.SetAttributes(ico, File.GetAttributes(ico) | FileAttributes.Hidden);
+                                File.SetAttributes(hid, File.GetAttributes(hid) | FileAttributes.Hidden | FileAttributes.System);
+
+                                // Making folder Read Only?
+                                //File.SetAttributes(dir, File.GetAttributes(dir) | FileAttributes.ReadOnly);
+
+                                //var tes = RandomNumber(0, 999);
+                                // Attempt 01 
+                                //Directory.Move(dir, dir + $"_Processing_{tes}");
+                                //Directory.Move(dir + $"_Processing_{tes}", dir);
+
+                                found++;
+                            }
+                            catch (Exception)
+                            {
+                                error++;
+                            }                            
+                        }
+                        else
+                        {
+                            error++;
+                        }
+                    }
+                }
+
+                // Reset Icon
+                //RefreshIcons();
+
+                log($"All Folder {all} | Icon OK {found} - Icon Error {error} | Remove Config {remove_config}");
+            }
+            else
+            {
+                log("~ No Found Folder");
+            }
+        }
+
+        public void log(string send)
+        {
+            Log.AppendText(send);
+            Log.AppendText(Environment.NewLine);
+        }
+
+        public bool RefreshIcons()
+        {
+            // Attempt 02
+            string localIconCachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\IconCache.db";
+            if (File.Exists(localIconCachePath))
+            {
+                File.Delete(localIconCachePath);
+            }
+            // Attempt 03
+            string dirCachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Microsoft\Windows\Explorer\";
+            DirectoryInfo di = new DirectoryInfo(dirCachePath);
+            FileInfo[] files = di.GetFiles("iconcache*.db");
+            foreach (FileInfo file in files)
+            {
+                File.Delete(file.FullName);
+            }
+            // Attempt 04.01
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C ie4uinit.exe -ClearIconCache";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            // Attempt 04.02
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C ie4uinit.exe -ClearIconCache";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            // Attempt 05
+            /*
+            foreach (Process p in Process.GetProcesses())
+            {
+                if (p.MainModule.ModuleName.Contains("explorer") == true)
+                {
+                    p.Kill();
+                }
+            }
+            Process.Start("explorer.exe");
+            */
+
+            // Attempt 06
+            // Copied from stackoverflow.com
+            SHChangeNotify(0x08000000, 0x0000, (IntPtr)null, (IntPtr)null);//SHCNE_ASSOCCHANGED SHCNF_IDLIST
+
+            return true;
+        }
+
+        public bool RetIcon(string dir, string icon,bool remove=false)
+        {
+            try
+            {
+                // desktop.ini
+                if (File.Exists(dir + @"\desktop.ini"))
+                {
+                    File.SetAttributes(dir + @"\desktop.ini", File.GetAttributes(dir + @"\desktop.ini") | FileAttributes.Normal); //Normal file
+                    FileInfo fileInfo = new FileInfo(dir + @"\desktop.ini")
+                    {
+                        IsReadOnly = false
+                    };
+                    File.Delete(dir + @"\desktop.ini");
+                }
+                // Icon.ico
+                if (File.Exists(dir + @"\" + icon))
+                {
+                    File.SetAttributes(dir + @"\" + icon, File.GetAttributes(dir + @"\" + icon) | FileAttributes.Normal); //Normal file
+                    FileInfo fileInfo = new FileInfo(dir + @"\" + icon)
+                    {
+                        IsReadOnly = false
+                    };
+                    if (remove)
+                    {
+                        File.Delete(dir + @"\" + icon);
+                    }
+                }
+                // .hidden
+                if (File.Exists(dir + @"\.hidden"))
+                {
+                    File.SetAttributes(dir + @"\.hidden", File.GetAttributes(dir + @"\.hidden") | FileAttributes.Normal); //Normal file
+                    FileInfo fileInfo = new FileInfo(dir + @"\.hidden")
+                    {
+                        IsReadOnly = false
+                    };
+                    File.Delete(dir + @"\.hidden");
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        // Instantiate random number generator.  
+        private readonly Random _random = new Random();
+
+        // Generates a random number within a range.      
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        // Copied from stackoverflow.com
+        [DllImport("shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void SHChangeNotify(int wEventId, int uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TypeIcon.SelectedIndex = 0;
+        }
+
+        private void bt_clear_cache_Click(object sender, EventArgs e)
+        {
+            RefreshIcons();
+        }
+    }
+}
